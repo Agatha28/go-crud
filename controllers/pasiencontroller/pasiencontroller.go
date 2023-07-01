8872c2dbb7e567704fe19401b99176110767eeda
Agatha Santoso <57290030+Agatha28@users.noreply.github.com>
2023-07-01 21:58:50 +0700

    update data

--- controllers/pasiencontroller/pasiencontroller.go
+++ controllers/pasiencontroller/pasiencontroller.go
@@ -1,8 +1,11 @@
 package pasiencontroller
 
 import (
+	"fmt"
 	"net/http"
 	"text/template"
+
+	"github.com/Agatha28/go-crud/entities"
 )
 
 func Index(response http.ResponseWriter, request *http.Request) {
@@ -15,11 +18,28 @@ func Index(response http.ResponseWriter, request *http.Request) {
 }
 
 func Add(response http.ResponseWriter, request *http.Request) {
-	temp, err := template.ParseFiles("views/pasien/add.html")
-	if err != nil {
-		panic(err)
+
+	if request.Method == http.MethodGet {
+		temp, err := template.ParseFiles("views/pasien/add.html")
+		if err != nil {
+			panic(err)
+		}
+		temp.Execute(response, nil)
+	} else if request.Method == http.MethodPost {
+		request.ParseForm()
+
+		var pasien entities.Pasien
+		pasien.NamaLengkap = request.Form.Get("nama_lengkap")
+		pasien.NIK = request.Form.Get("nik")
+		pasien.JenisKelamin = request.Form.Get("jenis_kelamin")
+		pasien.TempatLahir = request.Form.Get("tempat_lahir")
+		pasien.TanggalLahir = request.Form.Get("tanggal_lahir")
+		pasien.Alamat = request.Form.Get("alamat")
+		pasien.NoHP = request.Form.Get("no_hp")
+
+		fmt.Println(pasien)
 	}
-	temp.Execute(response, nil)
+
 }
 
 func Edit(response http.ResponseWriter, request *http.Request) {
---
+++ models/pasienmodel.go
@@ -0,0 +1,39 @@
+package models
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/Agatha28/go-crud/config"
+	"github.com/Agatha28/go-crud/entities"
+)
+
+type PasienModel struct {
+	conn *sql.DB
+}
+
+func NewPasienModel() *PasienModel {
+	conn, err := config.DBConnection()
+
+	if err != nil {
+		panic(err)
+	}
+
+	return &PasienModel{
+		conn: conn,
+	}
+}
+
+func (p *PasienModel) Create(pasien entities.Pasien) bool {
+	result, err := p.conn.Exec("insert into pasien (nama_lengkap,nik,jenis_kelamin, tempat_lahir, tanggal_lahir, alamat, no_hp) values(?,?,?,?,?,?,?)",
+		pasien.NamaLengkap, pasien.NIK, pasien.JenisKelamin, pasien.TempatLahir, pasien.TanggalLahir, pasien.Alamat, pasien.NoHp)
+
+	if err != nil {
+		fmt.Println(err)
+		return false
+	}
+
+	lastInsertId, _ := result.LastInsertId()
+
+	return lastInsertId > 0
+}
