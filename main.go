ab2acea108d9c5757f2b424cd58003ddb0f8fa5a
Agatha Santoso <57290030+Agatha28@users.noreply.github.com>
2023-07-01 10:50:42 +0700

    update data

--- main.go
+++ main.go
@@ -8,4 +8,9 @@ import (
 
 func main() {
 	http.HandleFunc("/", pasiencontroller.Index)
+	http.HandleFunc("/pasien", pasiencontroller.Index)
+	http.HandleFunc("pasien/index", pasiencontroller.Index)
+	http.HandleFunc("pasien/add", pasiencontroller.Add)
+	http.HandleFunc("pasien/edit", pasiencontroller.Edit)
+	http.HandleFunc("pasien/delete", pasiencontroller.Delete)
 }
