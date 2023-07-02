d6ce1d07f2679914f36faf39820e93eac47b2697
Agatha Santoso <57290030+Agatha28@users.noreply.github.com>
2023-07-02 11:08:25 +0700

    update data

--- libraries/validation.go
+++ libraries/validation.go
@@ -1,11 +1,28 @@
 package libraries
 
 import (
+	"github.com/go-playground/locales/en"
 	ut "github.com/go-playground/universal-translator"
 	"github.com/go-playground/validator/v10"
+	en_translations "github.com/go-playground/validator/v10/translations/en"
 )
 
 type Validation struct {
 	validate *validator.Validate
 	trans    ut.Translator
+}
+
+func NewValidation() *Validation {
+	translator := en.New()
+	uni := ut.New(translator, translator)
+
+	trans, _ := uni.GetTranslator("en")
+
+	validate := validator.New()
+	en_translations.RegisterDefaultTranslations(validate, trans)
+
+	return &Validation{
+		validate: validate,
+		trans:    trans,
+	}
 }
